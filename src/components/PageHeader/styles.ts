d3699b2551b8e8cea66c941deb6316e7f794e680
FlavianOMEGA <flavianmelquiades@gmail.com>
2020-08-06 16:29:14 -0300

    Aula #04

---
+++ src/components/PageHeader/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+    backgroundColor: '#8257e5',
+  },
+  topBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  tittle: {
+    color: '#fff',
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 24,
+    lineHeight: 32,
+    maxWidth: 160,
+    marginVertical: 40,
+  }
+})
+
+export default styles;
